1368065deb02ea190736d0c2e18140a34642b322
Fraulk <fratulker@gmail.com>
2021-01-20 17:22:24 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ shots }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => ({
+    shots: "",
+  }),
+  mounted() {
+    this.$axios
+      .get(
+        "https://raw.githubusercontent.com/originalnicodrgitbot/test-git-python/main/shotsdb.json"
+      )
+      .then((response) => {
+        this.shots = response.data;
+        console.log(this.shots);
+      });
+  },
+};
 </script>
 
 <style>
